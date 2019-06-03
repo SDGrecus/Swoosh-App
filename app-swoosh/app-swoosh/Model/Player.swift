fe8ead350c6843530642147305b6e4ca961ee6c0
Dmitrii Smirnov <Dmitry@MacBook-Air.local>
2019-06-03 23:15:28 +1000

    Store data

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Dmitry Smirnov on 3/6/19.
+//  Copyright © 2019 Dmitry Smirnov. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
